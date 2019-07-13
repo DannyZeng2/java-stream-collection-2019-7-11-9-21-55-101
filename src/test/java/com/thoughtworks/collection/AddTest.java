2d4588fba2a37dc83bcc29aa9db933f8cb3e42d2
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-13 15:57:53 +0800

    AddTest Pass

--- src/test/java/com/thoughtworks/collection/AddTest.java
+++ src/test/java/com/thoughtworks/collection/AddTest.java
@@ -10,7 +10,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class AddTest {
 
     @Test
-    public void should_get_sum_of_evens() {
+    public void should_get_sum_of_evens() {  //预计：1min 实际：3min
         //求leftBorder 和rightBorder之间的偶数和
         int leftBorder = 1;
         int rightBorder = 10;
@@ -20,19 +20,19 @@ public class AddTest {
         assertThat(add.getSumOfEvens(rightBorder, leftBorder)).isEqualTo(30);
     }
 
-//    @Test
-//    public void should_get_sum_of_odds() {
-//        //求leftBorder 和rightBorder之间的奇数和
-//        int leftBorder = 1;
-//        int rightBorder = 10;
-//
-//        Add add = new Add();
-//        assertThat(add.getSumOfOdds(leftBorder, rightBorder)).isEqualTo(25);
-//        assertThat(add.getSumOfOdds(rightBorder, leftBorder)).isEqualTo(25);
-//    }
+    @Test
+    public void should_get_sum_of_odds() { //预计：1min 实际：1min
+        //求leftBorder 和rightBorder之间的奇数和
+        int leftBorder = 1;
+        int rightBorder = 10;
+
+        Add add = new Add();
+        assertThat(add.getSumOfOdds(leftBorder, rightBorder)).isEqualTo(25);
+        assertThat(add.getSumOfOdds(rightBorder, leftBorder)).isEqualTo(25);
+    }
 
     @Test
-    public void should_get_triple_and_add_two() {
+    public void should_get_triple_and_add_two() {  //预计：3min 实际：3min
         //求数组中每个元素的3倍加2的和
         Integer[] array = new Integer[]{1, 5, 7, 11, 35, 67};
         List<Integer> arrayList = Arrays.asList(array);
@@ -43,7 +43,7 @@ public class AddTest {
     }
 
     @Test
-    public void should_get_triple_of_odd_and_add_two() {
+    public void should_get_triple_of_odd_and_add_two() { //预计：1min 实际：1min
         //求数组中奇数元素的3倍加2，偶数元素不变的数组
         Integer[] array = new Integer[]{1, 5, 7, 12, 11, 35, 54, 67, 70};
         List<Integer> arrayList = Arrays.asList(array);
@@ -91,7 +91,7 @@ public class AddTest {
 //    }
 
     @Test
-    public void should_return_average_of_even() {
+    public void should_return_average_of_even() {  //预计：1min 实际：4min
         //求数组中所有偶数的平均数
         Integer[] array = new Integer[]{1, 2, 3, 4, 5};
         List<Integer> arrayList = Arrays.asList(array);
@@ -116,7 +116,7 @@ public class AddTest {
 //    }
 
     @Test
-    public void should_return_unrepeated_elements_from_evens() {
+    public void should_return_unrepeated_elements_from_evens() {  //预计：1min 实际：1min
         //找出数组中的所有偶数，从中剔除重复数据并返回
         Integer[] array = new Integer[]{1, 2, 3, 4, 1, 5};
         List<Integer> arrayList = Arrays.asList(array);
